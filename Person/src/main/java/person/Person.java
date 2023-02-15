c6df5faec84e2b15068e702fc1c37076e4f894ed
junk <jaimemesa@uma.es>
2023-02-15 17:31:43 +0100

    person class integrated

--- Person/src/main/java/person/Person.java
+++ Person/src/main/java/person/Person.java
@@ -10,6 +10,18 @@ public class Person {
 
     private final String gender;
     public Person(String name, int age, String gender) {
+        if(name == null) {
+            throw new RuntimeException("Error. Introduzca un nombre");
+        }
+
+        if(age < 0) {
+            throw new RuntimeException("Error. Introduzca una edad valida");
+        }
+
+        if(!gender.equals("M") && !gender.equals("H")) {
+            throw new RuntimeException("Error. Genero no valido");
+        }
+
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -28,6 +40,39 @@ public class Person {
     }
 
     public double[] averageAgePerGender(List<Person> persons) {
-        return null;
+        double[] avergaAgePerGender = new double[2];
+
+        double menAgeSum = 0;
+        double womenAgeSum = 0;
+
+        int numberOfMen = 0;
+        int numberOfWomen = 0;
+
+        double averageMen = 0;
+        double averageWomen = 0;
+
+        for (Person person : persons) {
+            if(person.gender().equals("H")) {
+                menAgeSum += person.age;
+                numberOfMen++;
+            }
+            else if(person.gender().equals("M")) {
+                womenAgeSum += person.age;
+                numberOfWomen++;
+            }
+        }
+
+        if(numberOfMen != 0) {
+            averageMen = menAgeSum / numberOfMen;
+        }
+
+        if(numberOfWomen != 0) {
+            averageWomen = womenAgeSum / numberOfWomen;
+        }
+
+        avergaAgePerGender[0] = averageMen;
+        avergaAgePerGender[1] = averageWomen;
+
+        return avergaAgePerGender;
     }
 }
